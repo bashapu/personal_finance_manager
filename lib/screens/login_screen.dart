a5166cc8344b7e139f76e202d3df112c3b98de8f
Bharath Ashapu <bashapu1@student.gsu.edu>
2025-04-08 23:50:35 -0400

    Add Form Validations

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,10 +5,13 @@ import 'dashboard_screen.dart';
 import '../models/user.dart';
 import '../services/session_manager.dart';
 import 'package:uuid/uuid.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class LoginScreen extends StatelessWidget {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormBuilderState>();
 
   void _continueAsGuest(BuildContext context) {
     final guestUser = AppUser(id: Uuid().v4(), name: "Guest", email: "", password: "");
@@ -20,23 +23,25 @@ class LoginScreen extends StatelessWidget {
   }
 
   void _login(BuildContext context) async {
-    final db = await DatabaseHelper.instance.database;
-    final users = await db.query(
-      'users',
-      where: 'email = ? AND password = ?',
-      whereArgs: [_emailController.text, _passwordController.text],
-    );
-    if (users.isNotEmpty) {
-      final user = AppUser.fromMap(users.first);
-      SessionManager().loginUser(user);
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => DashboardScreen()),
+    if (_formKey.currentState?.saveAndValidate() ?? false) {
+      final db = await DatabaseHelper.instance.database;
+      final users = await db.query(
+        'users',
+        where: 'email = ? AND password = ?',
+        whereArgs: [_emailController.text, _passwordController.text],
       );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Invalid credentials")),
-      );
+      if (users.isNotEmpty) {
+        final user = AppUser.fromMap(users.first);
+        SessionManager().loginUser(user);
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => DashboardScreen()),
+        );
+      } else {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("Invalid credentials")));
+      }
     }
   }
 
@@ -46,26 +51,58 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Login')),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(controller: _emailController, decoration: InputDecoration(labelText: 'Email')),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(onPressed: () => _login(context), child: Text('Login')),
-            TextButton(
-              onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => RegisterScreen())),
-              child: Text('Register'),
-            ),
-            TextButton(
-              onPressed: () => _continueAsGuest(context),
-              child: Text('Continue as Guest'),
-            ),
-          ],
+        child: FormBuilder(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FormBuilderTextField(
+                name: 'email',
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: "Email"),
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(),
+                  FormBuilderValidators.email(),
+                ]),
+              ),
+              FormBuilderTextField(
+                name: 'password',
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: "Password"),
+                obscureText: true,
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(),
+                  FormBuilderValidators.minLength(
+                    6,
+                    errorText: 'Password must be at least 6 characters',
+                  ),
+                ]),
+              ),
+              //TextField(controller: _emailController, decoration: InputDecoration(labelText: 'Email')),
+              // TextField(
+              //   controller: _passwordController,
+              //   decoration: InputDecoration(labelText: 'Password'),
+              //   obscureText: true,
+              // ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _login(context),
+                child: Text('Login'),
+              ),
+              TextButton(
+                onPressed:
+                    () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => RegisterScreen()),
+                    ),
+                child: Text('Register'),
+              ),
+              TextButton(
+                onPressed: () => _continueAsGuest(context),
+                child: Text('Continue as Guest'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -4,26 +4,31 @@ import 'dashboard_screen.dart';
 import '../services/session_manager.dart';
 import '../models/user.dart';
 import 'package:uuid/uuid.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class RegisterScreen extends StatelessWidget {
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormBuilderState>();
 
   void _register(BuildContext context) async {
-    final user = AppUser(
-      id: Uuid().v4(),
-      name: _nameController.text,
-      email: _emailController.text,
-      password: _passwordController.text,
-    );
-    final db = await DatabaseHelper.instance.database;
-    await db.insert('users', user.toMap());
-    SessionManager().loginUser(user);
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (_) => DashboardScreen()),
-    );
+    if (_formKey.currentState?.saveAndValidate() ?? false) {
+      final user = AppUser(
+        id: Uuid().v4(),
+        name: _nameController.text,
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+      final db = await DatabaseHelper.instance.database;
+      await db.insert('users', user.toMap());
+      SessionManager().loginUser(user);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => DashboardScreen()),
+      );
+    }
   }
 
   @override
@@ -32,14 +37,48 @@ class RegisterScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Register')),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            TextField(controller: _nameController, decoration: InputDecoration(labelText: 'Full Name')),
-            TextField(controller: _emailController, decoration: InputDecoration(labelText: 'Email')),
-            TextField(controller: _passwordController, decoration: InputDecoration(labelText: 'Password',), obscureText: true,),
-            SizedBox(height: 20),
-            ElevatedButton(onPressed: () => _register(context), child: Text('Register')),
-          ],
+        child: FormBuilder(
+          key: _formKey,
+          child: Column(
+            children: [
+              FormBuilderTextField(
+                name: 'name',
+                decoration: const InputDecoration(labelText: "Full Name"),
+                controller: _nameController,
+                validator: FormBuilderValidators.required(),
+              ),
+              // TextField(controller: _nameController, decoration: InputDecoration(labelText: 'Full Name')),
+              FormBuilderTextField(
+                name: 'email',
+                decoration: const InputDecoration(labelText: "Email"),
+                controller: _emailController,
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(),
+                  FormBuilderValidators.email(),
+                ]),
+              ),
+              // ßTextField(controller: _emailController, decoration: InputDecoration(labelText: 'Email')),
+              FormBuilderTextField(
+                name: 'password',
+                decoration: const InputDecoration(labelText: "Password"),
+                controller: _passwordController,
+                obscureText: true,
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(),
+                  FormBuilderValidators.minLength(
+                    6,
+                    errorText: 'Password must be at least 6 characters',
+                  ),
+                ]),
+              ),
+              // TextField(controller: _passwordController, decoration: InputDecoration(labelText: 'Password',), obscureText: true,),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _register(context),
+                child: Text('Register'),
+              ),
+            ],
+          ),
         ),
       ),
     );
